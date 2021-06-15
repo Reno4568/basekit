f59db12ddac43ffa5ccff79f6b372845ca46326c
Angga Risky <anggariskysetiawan@gmail.com>
2021-06-15 19:41:44 +0700

    login validate

--- resources/views/pieces/navbar.blade.php
+++ resources/views/pieces/navbar.blade.php
@@ -25,9 +25,41 @@
               <li><a class="dropdown-item" href="#">Request Design</a></li>
             </ul>
           </li>
+          @if (session()->has('user'))
+            <ul class="navbar-nav ml-auto dropdown navbar-nav--profile">
+                <li>
+                    <form action="{{ route('search') }}" method="GET" role="search">
+                        <div class="input-group">
+                            <div class="form-outline">
+                                <input type="search" class="form-control h-100" id="exampleFormControlInput1" name="q" placeholder="Cari pixel...">
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-search fa-xs"></i>
+                            </button>
+                        </div>
+                    </form>
+                </li>
+                <a href="#" class="btn btn-profile" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <img src="{{ asset('images/pic_default.svg') }}"
+                        alt="{{ session()->get('user.name') }} from Pixel by BuildWith Angga">
+                    <p>
+                        Howdy, {{ session()->get('user.name') }}!
+                    </p>
+                </a>
+                <div class="dropdown-menu ml-auto" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('cms-product.index') }}">Dashboard</a>
+                    <a class="dropdown-item" href="#">My Profile</a>
+                    <a class="dropdown-item" href="#">Settings</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="{{ url('/auth/logout') }}">Logout</a>
+                </div>
+            </ul>
+            @else
           <li class="nav-item">
-            <a class="btn btn-primary" href="#">Login</a>
+            <a class="btn btn-primary" href="{{ url('/gate') }}">Login</a>
           </li>
+          @endif
         </ul>
       </div>
     </div>
