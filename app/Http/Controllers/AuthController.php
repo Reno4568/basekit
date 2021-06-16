02214e7e9c22940bae87cb76e6d6bb4e96c1e604
Achmad Fauzi <achmadfauzi@jiboncoco.local>
2021-06-16 12:36:53 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 use Socialite;
 use Auth;
+
 use App\Models\User;
 use App\Models\RegisteredUser;
 use App\Services\AuthService;
@@ -13,24 +16,57 @@ class AuthController extends Controller
 {
     public function redirectToGoogle()
     {
+        // redirect to google
         return Socialite::driver('google')->redirect();
     }
 
     public function callback(Request $request)
     {
         try {
+            
+            // checking google socialite - get data google
             $user = Socialite::driver('google')->user();
+            $find_user = User::where('google_id', $user->getId())->first();
+
+            if($find_user){
+
+                // login process
+                Auth::login($find_user);
+                return redirect()->route('index');
+
+            }else{
+
+                // register new user
+                $new_user = new User;
+                $new_user->name = $user->getName();
+                $new_user->email = $user->getEmail();
+                $new_user->password = Hash::make($user->getId());
+                $new_user->google_id = $user->getId();
+                $new_user->save();
+
+                // login process
+                Auth::login($find_user);
+                return redirect()->route('index');
+
+            }
+
         } catch (\Exception $e) {
-            return redirect('/gate')->with('error', 'Gagal menyambungkan dengan akun Google kamu');
+
+            // return to gate
+            return redirect()->route('gate')
+                            ->withErrors('Email tersebut sudah terdaftar, silahkan login dengan email lain')
+                            ->withInput();
+            // return redirect('/gate')->with('error', 'Gagal menyambungkan dengan akun Google kamu');
+            
         }
 
-        $payload = ['provider_id' => $user->id, 'email' => $user->email, 'provider_name' => 'google'];
-        $user = AuthService::login($payload);
-        if ($user) {
-            return $this->successLogin($user, $request);
-        }
+        // $payload = ['provider_id' => $user->id, 'email' => $user->email, 'provider_name' => 'google'];
+        // $user = AuthService::login($payload);
+        // if ($user) {
+        //     return $this->successLogin($user, $request);
+        // }
 
-        return redirect('/gate')->with('error', 'Kamu belum terdaftar sebagai member BWA!');
+        // return redirect('/gate')->with('error', 'Kamu belum terdaftar sebagai member BWA!');
     }
 
     public function login(Request $request)
