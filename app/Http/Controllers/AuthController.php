fc0444591a5b1775c713ea3b0d54e31947ca9fc5
Achmad Fauzi <achmadfauzi@jiboncoco.local>
2021-06-16 13:22:25 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 use Laravel\Socialite\Facades\Socialite;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 use App\Models\RegisteredUser;
